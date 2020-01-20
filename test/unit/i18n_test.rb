d9def51a8efa4456cf431a4a6ff825c217bc3a4a
Alexander Meindl <alex@alphanodes.com>
2020-01-20 18:07:23 +0100

    Fix strings for tests

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -17,11 +17,7 @@ class I18nTest < RedmineCodimd::TestCase
   end
 
   def test_locales_validness
-    lang_files_count = Dir[Rails.root.join('plugins',
-                                           'redmine_codimd',
-                                           'config',
-                                           'locales',
-                                           '*.yml')].size
+    lang_files_count = Dir[Rails.root.join('plugins/redmine_codimd/config/locales/*.yml')].size
     assert_equal lang_files_count, 2
     valid_languages.each do |lang|
       assert set_language_if_valid(lang)
